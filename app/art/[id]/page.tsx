ab6b2e4afde5062cd432223dbd98c4b1d6881c78
Jeff Soriano <soriano.jeffm@gmail.com>
2025-08-19 21:32:37 -0700

    Add skeleton loader to art page

--- app/art/[id]/page.tsx
+++ app/art/[id]/page.tsx
@@ -19,10 +19,12 @@ export default function ArtPage({
 
     const [artData, setArtData] = useState<ArticArtDetailsType | null>(null)
     const [artConfig, setArtConfig] = useState<ArticConfigType | null>(null)
+    const [isLoading, setIsLoading] = useState(true)
     const { data, config } = useArticContext()
 
     useEffect(() => {
         const fetchArtData = async () => {
+            setIsLoading(true)
             const artData = data.find((art) => art.id === parseInt(id)) as
                 | ArticArtDetailsType
                 | undefined
@@ -35,11 +37,51 @@ export default function ArtPage({
                 setArtData(artData)
                 setArtConfig(config)
             }
+            setIsLoading(false)
         }
 
         fetchArtData()
     }, [data, config, id])
 
+    // Skeleton loading state
+    if (isLoading)
+        return (
+            <div className="w-full flex flex-col gap-2 p-4 md:w-3/4 mx-auto lg:max-w-4xl">
+                <Link
+                    href="/"
+                    className="text-sm text-gray-500 hover:underline"
+                >
+                    &larr; Back to search
+                </Link>
+
+                {/* Skeleton Layout */}
+                <div className="flex flex-col gap-8">
+                    {/* Image Skeleton */}
+                    <div className="w-full min-h-[300px] relative aspect-[4/3] mx-auto animate-pulse">
+                        <div className="rounded-lg bg-gray-200 h-full dark:bg-gray-700"></div>
+                    </div>
+
+                    {/* Content Skeleton */}
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 animate-pulse">
+                        {/* First column Skeleton */}
+                        <div className="space-y-2">
+                            <div className="h-6 bg-gray-200 dark:bg-gray-700 rounded animate-pulse w-2/3"></div>
+                            <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded animate-pulse w-1/3"></div>
+                            <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded animate-pulse w-1/3"></div>
+                        </div>
+                        {/* Second column Skeleton */}
+                        <div className="space-y-3">
+                            <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded animate-pulse"></div>
+                            <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded animate-pulse w-5/6"></div>
+                            <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded animate-pulse w-4/5"></div>
+                            <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded animate-pulse w-3/4"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+
+    // Art not found
     if (!artData || !artConfig)
         return (
             <div className=" text-center w-full flex flex-col gap-2 p-4 md:w-3/4 mx-auto lg:max-w-4xl">
@@ -53,13 +95,14 @@ export default function ArtPage({
             </div>
         )
 
+    // Art found
     return (
         <div className="w-full flex flex-col gap-2 p-4 md:w-3/4 mx-auto lg:max-w-4xl">
             <Link href="/" className="text-sm text-gray-500 hover:underline">
                 &larr; Back to search
             </Link>
 
-            <div className="w-full min-h-[300px] relative aspect-[4/3] mx-auto">
+            <div className="w-full min-h-[300px] relative aspect-[4/3] mx-auto animate-[fadeIn_1s_ease-in-out]">
                 <Image
                     src={`${artConfig.iiif_url}/${artData.image_id}/full/843,/0/default.jpg`}
                     alt={
