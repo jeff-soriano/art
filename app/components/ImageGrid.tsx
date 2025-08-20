1011d0ba5a4e73c18ba21e7f284a249cf84d27d9
Jeff Soriano <soriano.jeffm@gmail.com>
2025-08-19 21:14:09 -0700

    Add fade in animation and loading skeleton to image grid

--- app/components/ImageGrid.tsx
+++ app/components/ImageGrid.tsx
@@ -15,41 +15,53 @@ function ImageGridImpl({ data, config, isLoading }: Props) {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4 p-4 lg:w-4/5 mx-auto">
             {config &&
                 data &&
-                data.map(({ id, image_id, thumbnail, title, artist_title }) => {
-                    return (
-                        <div
-                            key={id}
-                            className="relative w-full min-h-[200px] aspect-[4/3] group cursor-pointer"
-                        >
-                            <Link
-                                className="w-full h-full relative block"
-                                href={`art/${id}`}
+                data.map(
+                    (
+                        { id, image_id, thumbnail, title, artist_title },
+                        index
+                    ) => {
+                        return (
+                            <div
+                                key={id}
+                                className="relative w-full min-h-[200px] aspect-[4/3] group cursor-pointer animate-[fadeIn_1s_ease-in-out]"
                             >
-                                <Image
-                                    src={`${config.iiif_url}/${image_id}/full/400,/0/default.jpg`}
-                                    alt={
-                                        thumbnail?.alt_text ||
-                                        `${title} by ${artist_title}`
-                                    }
-                                    fill
-                                    className="object-cover rounded-lg"
-                                />
-                                <div className="absolute bottom-0 left-0 right-0 p-2 bg-black/50 text-white rounded-b-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-                                    <p className="text-sm line-clamp-1">
-                                        {title}
-                                    </p>
-                                    <p className="text-xs line-clamp-1">
-                                        {artist_title}
-                                    </p>
-                                </div>
-                            </Link>
-                        </div>
-                    )
-                })}
+                                <Link
+                                    className="w-full h-full relative block"
+                                    href={`art/${id}`}
+                                >
+                                    <Image
+                                        src={`${config.iiif_url}/${image_id}/full/400,/0/default.jpg`}
+                                        alt={
+                                            thumbnail?.alt_text ||
+                                            `${title} by ${artist_title}`
+                                        }
+                                        fill
+                                        className="object-cover rounded-lg"
+                                    />
+                                    <div className="absolute bottom-0 left-0 right-0 p-2 bg-black/50 text-white rounded-b-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+                                        <p className="text-sm line-clamp-1">
+                                            {title}
+                                        </p>
+                                        <p className="text-xs line-clamp-1">
+                                            {artist_title}
+                                        </p>
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    }
+                )}
             {isLoading && (
-                <div className="col-span-full flex justify-center items-center">
-                    <FaSpinner className="w-10 h-10 animate-spin" />
-                </div>
+                <>
+                    {Array.from({ length: 50 }).map((_, index) => (
+                        <div
+                            key={`skeleton-${index}`}
+                            className="relative w-full h-48 aspect-[4/3] group animate-pulse"
+                        >
+                            <div className="flex items-center justify-center w-full h-full bg-gray-200 dark:bg-gray-700 rounded-lg"></div>
+                        </div>
+                    ))}
+                </>
             )}
         </div>
     )
